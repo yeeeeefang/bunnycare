b8485067a191f03abbf752ae7a113106c7f31560
yeeeeefang <sweetcat881314@gmail.com>
2025-01-14 17:53:14 +0800

    醫療

--- src/components/homeother/HomeCarousel.jsx
+++ src/components/homeother/HomeCarousel.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React,{ useEffect, useState } from 'react';
 import { IoIosArrowDroprightCircle } from "react-icons/io";
 import { IoIosArrowDropleftCircle } from "react-icons/io";
 import './HomeCarousel.scss';
--- src/pages/medical/Medical.jsx
+++ src/pages/medical/Medical.jsx
@@ -1,8 +1,15 @@
-
-// import './Medical.scss';
-import '../css/Medical.css'
+import MedicalShow from '../../components/medicalother/MedicalShow';
+import './Medical.scss';
+// import '../css/Medical.css'
 function Medical() {
-    return <h2>這是醫療頁</h2>;
-  }
-  
-  export default Medical;
+  return (
+    <>
+      <h2>這是醫療頁</h2>
+      <div className='show'> 
+        <MedicalShow />
+      </div>
+    </>
+  );
+}
+
+export default Medical;
