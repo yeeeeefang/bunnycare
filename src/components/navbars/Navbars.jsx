794714dc320a9a955e7fcdcc1326f538df495f56
yeeeeefang <sweetcat881314@gmail.com>
2025-01-20 10:45:42 +0800

    new

--- src/components/navbars/Navbars.jsx
+++ src/components/navbars/Navbars.jsx
@@ -8,7 +8,7 @@ const navLinks = [
   { name: "首頁", path: "/home" },
   { name: "醫療資訊", path: "/medical" },
   { name: "知識區", path: "/knowledge" },
-  { name: "我的會員", path: "/about" },
+  // { name: "我的會員", path: "/about" },
 ];
 
 export default function Navbars() {
